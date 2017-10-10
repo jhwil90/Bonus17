73c829895aecfa08d9334b092b2136f4e3b05977
jhwil90 <31808102+jhwil90@users.noreply.github.com>
2017-10-10 08:01:04 -0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus17
+{
+    class Car
+    {
+        //Methods
+        public void Cars()
+        {
+            Console.WriteLine("Please enter the cars make.");
+            Make = Console.ReadLine();
+
+            Console.WriteLine("Please enter the car's model.");
+            Model = Console.ReadLine();
+
+            Console.WriteLine("Please enter the year of the car.");
+            Year = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the car's price.");
+            Price = double.Parse(Console.ReadLine()); }
+
+
+        //Members
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        // property
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+
+        }
+
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+
+        //Constructors
+        public Car() 
+        {
+            make = "";
+            model = "";
+            year = 0;
+            price = 0;
+
+            //Constructors
+
+            public void Cars(string CarMake, string CarModel, int CarYear, double CarPrice);
+            {
+                model = "CarModel";
+                make = "CarMake";
+                price = Int32.Parse(("CarPrice"));
+                year = double.(("CarPrice"));
+
+            }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ContinueProgram = true;
+            while (ContinueProgram == true)
+            {
+                int NumOfCars;
+                Car CarDescription = new Car();
+                Console.WriteLine("Welcome to the Grand Circus Motors admin console!");
+                Console.WriteLine("How many cars are you entering?");
+                NumOfCars = int.Parse(Console.ReadLine());
+                Car[] Inventory = new Car[NumOfCars];
+
+               
+                for (int i = 0; i < Inventory.Length; i++)
+                {
+
+                    Console.WriteLine("Please enter the Make of the car:");
+                    Inventory[i].Make = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the Model of the car:");
+                    Inventory[i].Model = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the Year of the car:");
+                    Inventory[i].Year = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the Price of the car:");
+                    Inventory[i].Price = double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Current Inventory:");
+                Console.WriteLine("Make \ tModel \ tYear \ t Price");
+                foreach (Car UserSelection in Inventory)
+                {
+                    Console.WriteLine($"{UserSelection.Make} \t{UserSelection.Model} \t{UserSelection.Year} \t{UserSelection.Price}");
+                }
+
+                string UserChoice; 
+                Console.WriteLine("Continue? (y/n)");
+                UserChoice = Console.ReadLine();
+
+                if (UserChoice == "N" || UserChoice == "No" || UserChoice == "no" || UserChoice == "n" && UserChoice != "yes" || UserChoice != "y")
+                {
+                    ContinueProgram = false;
+                    Console.WriteLine("Goodbye.");
+                }
+                Console.ReadKey();
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
